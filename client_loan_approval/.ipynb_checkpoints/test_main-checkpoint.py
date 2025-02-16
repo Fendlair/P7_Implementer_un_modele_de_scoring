09fd7f1c9118b30473ec27a95e870a4b875003f4
Etienne Rebuffet <etienne.rebuffet@orange.fr>
2025-02-16 19:18:09 +0100

    Updated folders hierarchy

---
+++ client_loan_approval/.ipynb_checkpoints/test_main-checkpoint.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 14 08:44:37 2025
+
+@author: etienne
+"""
+
+import unittest
+import requests
+import json
+
+# FastAPI API URL
+API_URL = "http://127.0.0.1:8000/predict"
+
+class TestPredictionAPI(unittest.TestCase):
+
+    # Test if inputs are numericals values
+    def test_input_data_type(self):
+        # Input data (Only numericals)
+        valid_data = {
+            "features": [-11577, -3287, 0.76, 0.0, 6.0, 0.0, 0.0, -2118.0, -1480.0, 0.0, 0.0, 0.0, 1.0, -109.0, 11.43]
+        }
+
+        # Input data (Containing a string)
+        invalid_data = {
+            "features": ["etienne", -3287, 0.76, 0.0, 6.0, 0.0, 0.0, -2118.0, -1480.0, 0.0, 0.0, 0.0, 1.0, -109.0, 11.43]
+        }
+
+        # Test API with numerical data
+        response = requests.post(API_URL, json=valid_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIsInstance(response.json()["probability"], float)
+
+        # Test data with numerical and str data
+        response = requests.post(API_URL, json=invalid_data)
+        self.assertEqual(response.status_code, 422)  # Error code for invalide data
+        
+    # Testing length of input data
+    def test_data_length(self):
+        # Data with right length
+        correct_length_data = {
+            "features": [-11577, -3287, 0.76, 0.0, 6.0, 0.0, 0.0, -2118.0, -1480.0, 0.0, 0.0, 0.0, 1.0, -109.0, 11.43]
+        }
+
+        # Data with shorter length
+        incorrect_length_data = {
+            "features": [-11577, -3287, 0.76, 0.0, 6.0, 0.0, 0.0, -2118.0, -1480.0, 0.0, 0.0, 0.0, 1.0, -109.0]
+        }
+
+        # Test if data of the right length
+        response = requests.post(API_URL, json=correct_length_data)
+        self.assertEqual(response.status_code, 200)
+
+        # Test if data length is not right
+        response = requests.post(API_URL, json=incorrect_length_data)
+        self.assertEqual(response.status_code, 422)  # Error code for invalide data
+
+    # Test answer of the API
+    def test_prediction_response(self):
+        # Input data
+        valid_data = {
+            "features": [-11577, -3287, 0.76, 0.0, 6.0, 0.0, 0.0, -2118.0, -1480.0, 0.0, 0.0, 0.0, 1.0, -109.0, 11.43]
+        }
+
+        # Prediction request
+        response = requests.post(API_URL, json=valid_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn("probability", response.json())
+        self.assertIsInstance(response.json()["probability"], float)
+
+# Run test
+if __name__ == "__main__":
+    unittest.main()
---
+++ client_loan_approval/app/main.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 11 10:37:21 2025
+
+@author: etienne
+"""
+
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from .model import load_model
+
+# PATH to the model.pkl
+model_path = "model/model.pkl"
+
+# Loading the model
+model = load_model(model_path)
+
+# Application FastAPI
+app = FastAPI()
+
+# Data shape for prediction
+class PredictionRequest(BaseModel):
+	features: list = Field(..., min_items=15, max_items=15,
+	                       description="List of 15 informations about the client.")
+
+# Data for answer
+class PredictionResponse(BaseModel):
+    probability: float
+
+# End point for prediction
+@app.post("/predict", response_model=PredictionResponse)
+def predict(request: PredictionRequest):
+    try:
+        # Faire la prédiction
+        prediction_proba = model.predict_proba([request.features])[0]
+
+        return PredictionResponse(probability=prediction_proba[1])
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Erreur lors de la prédiction.")
+
+# Define a function to return a description of the app
+def get_app_description():
+	return (
+    	"Welcome to the Client loan aprouval API!"
+    	"This API allows you to determine if we should allow a credit to a client based on some data."
+    	"Use the '/predict/' endpoint with a POST request to make predictions."
+    	"Example usage: POST to '/predict/' with JSON data containing"
+	)
+
+# Define the root endpoint to return the app description
+@app.get("/")
+async def root():
+	return {"message": get_app_description()}
