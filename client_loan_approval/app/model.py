c631dc07c437c6aac805bfcbd96d0b7eca88a8f3
Etienne Rebuffet <etienne.rebuffet@orange.fr>
2025-02-17 10:48:18 +0100

    Changed how the model is loaded in order the app to work on github action

--- client_loan_approval/app/model.py
+++ client_loan_approval/app/model.py
@@ -1,5 +1,14 @@
+import os
 import joblib
 
+
 def load_model(model_path):
-    model = joblib.load(model_path)
+    # Absolut path to the model
+    absolute_path = os.path.abspath(os.path.join(os.path.dirname(__file__), model_path))
+    print('Loading model from:', absolute_path)
+
+    # Load the model
+    with open(absolute_path, 'rb') as file:
+        model = joblib.load(file)
+
     return model
