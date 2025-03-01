947d393cd831f5f844282890012b9bba9d4df489
Etienne Rebuffet <etienne.rebuffet@orange.fr>
2025-03-01 21:48:15 +0100

    Cleaned dashbord

--- client_loan_approval/app/main.py
+++ client_loan_approval/app/main.py
@@ -5,7 +5,7 @@ Created on Tue Feb 11 10:37:21 2025
 
 @author: etienne
 """
-
+import os
 from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel, Field, ValidationError
 from .model import load_model
@@ -54,8 +54,6 @@ def predict(request: PredictionRequest):
         # Shap values calculation
         shap_values = explainer(pipeline.named_steps["scaler"].transform([features]))
         shap_values_list = shap_values.values[0].tolist()
-        print(prediction_proba)
-        print("shap_values:", shap_values_list)
 
         return PredictionResponse(probability=prediction_proba[1], shap_values=shap_values_list)
 
@@ -85,3 +83,7 @@ async def root():
 async def validation_exception_handler(request: Request, exc: ValidationError):
     logger.error(f"Erreur de validation: {exc}")
     return JSONResponse(status_code=422, content={"detail": exc.errors()})
+
+if __name__== "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
